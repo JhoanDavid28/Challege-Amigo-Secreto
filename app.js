8487df73dfe6b0dbda2c940f84ac68ae334c1255
Jhoan <pjhoandavid@outlook.com>
2025-03-08 01:17:35 -0500

    Agregado funcion para agregar amigo

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let = amigo = [];
+
+function agregarAmigo(){
+    let input_amigo = document.getElementById("amigo");
+    let amigo = input_amigo.value;
+
+    console.log(amigo);
+}
